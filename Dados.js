db76c9177b25576001e31afd278b22163d05dfe7
Reinaldo-S-Silva <jp.renbs@gmail.com>
2024-09-07 15:08:58 -0300

    Add files via upload

---
+++ Dados.js
@@ -0,0 +1,27 @@
+let dados = [
+    {
+
+        titulo: "Gênesis",
+        descricao: "O livro de Gênesis é a pedra angular da Bíblia, apresentando a narrativa fundamental da criação do universo, da humanidade e dos eventos que moldaram as civilizações antigas. Ele explora temas profundos como a origem do bem e do mal, a relação entre Deus e o homem, e as promessas divinas para a humanidade.",
+        link: "https://pt.wikipedia.org/wiki/Gênesis",
+        tags: "inicio Comerco criacao"
+    },
+
+    {
+
+        titulo: "Êxodo",
+        descricao: "O livro de Êxodo narra a história da libertação do povo hebreu do Egito, liderados por Moisés. Aborda temas como as pragas do Egito, a travessia do Mar Vermelho e a entrega das Tábuas da Lei no Monte Sinai.",
+        link: "https://pt.wikipedia.org/wiki/Livro_do_Êxodo",
+        tags: "saida saída livramento"
+    },
+
+    {
+
+        titulo: "Levítico",
+        descricao: "O livro de Levítico contém as leis e regulamentos religiosos dados por Deus ao povo de Israel, especialmente relacionados ao culto e à santidade. Aborda temas um pouco mais complexos, como os sacrifícios, a pureza ritual e as leis alimentares.",
+        link: "https://pt.wikipedia.org/wiki/Levítico",
+        tags: "leis sacrificios pureza"
+    },
+    
+];
+
